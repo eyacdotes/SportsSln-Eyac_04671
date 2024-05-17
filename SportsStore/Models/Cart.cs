7895e78a836dd783ca369d48ed49b22d7867aa17
Eyac <eyaclemarc@gmail.com>
2024-05-17 11:32:29 +0800

    Initial commit

---
+++ SportsStore/Models/Cart.cs
@@ -0,0 +1,42 @@
+﻿namespace SportsStore.Models
+{
+    public class Cart
+    {
+        public List<CartLine> Lines { get; set; } = new List<CartLine>();
+
+        public virtual void AddItem(Product product, int quantity)
+        {
+            CartLine? line = Lines
+                .Where(p => p.Product.ProductId == product.ProductId)
+                .FirstOrDefault();
+
+            if (line == null)
+            {
+                Lines.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public virtual void RemoveLine(Product product) =>
+            Lines.RemoveAll(l => l.Product.ProductId == product.ProductId);
+
+        public decimal ComputeTotalValue() =>
+            Lines.Sum(e => e.Product.Price * e.Quantity);
+
+        public virtual void Clear() => Lines.Clear();
+    }
+
+    public class CartLine
+    {
+        public int CartLineID { get; set; }
+        public Product Product { get; set; } = new Product();
+        public int Quantity { get; set; }
+    }
+}
